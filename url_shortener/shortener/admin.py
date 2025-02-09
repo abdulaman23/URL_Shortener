a3f2c2e3e5fb46d362812cccaca9fa35b286ff07
Abdul Aman <kshmr.lg@gmail.com>
2025-02-09 22:11:58 +0400

    Added Model and Admin panel files

--- url_shortener/shortener/admin.py
+++ url_shortener/shortener/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import URL
 
 # Register your models here.
+
+
+class URLAdmin(admin.ModelAdmin):
+    list_display = ('short_url', 'long_url', 'created_at')
+
+admin.site.register(URL, URLAdmin)
+admin.site.site_header = "URL Shortener Admin"
--- url_shortener/shortener/models.py
+++ url_shortener/shortener/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+import shortuuid
 # Create your models here.
+
+class URL(models.Model):
+    short_url = models.CharField(max_length=10, unique=True, primary_key=True)
+    long_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.short_url:
+            self.short_url = shortuuid.uuid()[:8]
+        super().save(*args,**kwargs)
+
+    
+    def __str__(self):
+        return f"{self.short_url} -> {self.long_url}"
